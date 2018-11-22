53f220225283b3cd1229a84379c3261a7c0d2eac
Christophe NOUJAIM <christophe@noujaim.com>
2018-11-22 17:49:32 +0100

    rake green

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, presence: true, uniqueness: { scope: [:cocktail,
-    :ingredient] }
+  validates :description, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
